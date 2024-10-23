e8dbf7b11729939c1689852aaa1566d55539c804
Puspal Paul <puspal.paul@gcettb.ac.in>
2024-10-23 19:52:06 +0530

    next commit

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+const { defineConfig } = require('vite');
+
+module.exports = defineConfig({
+  plugins: [react()],
+  define: {
+    "process.env": {},
+    global: {},
+  },
+  resolve: {
+    alias: {
+      process: "process/browser",
+      stream: "stream-browserify",
+      zlib: "browserify-zlib",
+      util: "util",
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ["fsevents"],
+    },
+  },
+  optimizeDeps: {
+    include: ["buffer"],
+  },
+});
